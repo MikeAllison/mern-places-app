1923db81b568f4bd63260cf4cd5171ac2543794c
m <mikeallison79@gmail.com>
2020-12-28 16:25:46 -0500

    Basic app layout

---
+++ src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
+
+import './SideDrawer.css';
+
+const SideDrawer = (props) => {
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
+
+  // Hooks the mobile nav to a div outside of the root div
+  return ReactDOM.createPortal(
+    content,
+    document.getElementById('side-drawer-hook')
+  );
+};
+
+export default SideDrawer;
