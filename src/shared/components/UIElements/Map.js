fe987ec76283460ee9dfafd7f30174b456ade8e9
m <mikeallison79@gmail.com>
2020-12-28 19:17:33 -0500

    Added Google Map to project

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect } from 'react';
+
+import './Map.css';
+
+const Map = (props) => {
+  // Creates a pointer to the map div that survives re-renders
+  const mapRef = useRef();
+
+  // Extract only the props that are needed
+  const { center, zoom } = props;
+
+  // This is required to wait for this compopent to render the map until the DOM is rendered
+  // Pass extracted props as second arguement
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom,
+    });
+
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
+};
+
+export default Map;
